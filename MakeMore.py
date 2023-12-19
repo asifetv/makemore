b1be685f1f87e99f9a83746709a5bda6442debf9
Asif ETV <asifetv@gmail.com>
2023-12-18 21:41:37 -0800

    makemore checkin

---
+++ MakeMore.py
@@ -0,0 +1,65 @@
+import torch
+import matplotlib.pyplot as plt
+
+words = open("names.txt", 'r').read().splitlines()
+
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+
+itos = {i:s for s,i in stoi.items()}
+N = torch.zeros((27,27), dtype=torch.int32)
+
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        N[ix1, ix2] += 1
+
+#print(N[0, :])
+
+g = torch.Generator().manual_seed(2147483647)
+
+P = (N+1).float()
+P /= P.sum(1, keepdim=True)
+log_likelihood = 0
+n = 0
+
+#for w in words:
+for w in ["andrejq"]:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip (chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        prob = P[ix1, ix2]
+        logprob = torch.log(prob)
+        log_likelihood += logprob
+        n += 1
+        #print(f"{ch1, ch2}: {prob:.4f}")
+
+#print(f'{log_likelihood=}')
+nll = -log_likelihood
+#print(f'{nll=}')
+print(f'{nll/n}')
+
+for i in range (0):
+    ix = 0
+    out = []
+    while True:
+        p = P[ix]
+        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+        out.append(itos[ix])
+        if ix == 0:
+            break
+    print(''.join(out))
+
+plt.figure(figsize=(16,16))
+plt.imshow(N, cmap='Blues')
+for i in range(27):
+    for j in range(27):
+        chstr = itos[i] + itos[j]
+        plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
+        plt.text(j, i, N[i, j].item(), ha="center", va="top", color="gray")
+plt.axis('off')
+#plt.show()
